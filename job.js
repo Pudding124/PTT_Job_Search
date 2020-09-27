6572732e6efad451209f30a97a21d214db8ca33f
許銘仁 <surprised128@gmail.com>
2020-09-27 11:02:37 +0800

    2020-09-27 - version 1.0.0

---
+++ job.js
@@ -0,0 +1,59 @@
+// iframe
+var body = document.createElement("div");
+document.body.appendChild(body);
+body.setAttribute("id", "Job");
+// 自由移動
+dragElement(body);
+
+const iframe = document.createElement('iframe');
+iframe.id = 'iframe';
+iframe.sandbox = 'allow-scripts allow-same-origin allow-forms';
+iframe.frameBorder = "0";
+iframe.src = chrome.runtime.getURL('popup.html');
+body.prepend(iframe);
+
+const header = document.createElement('div');
+header.setAttribute("id", "header");
+body.prepend(header);
+
+// 設定自由移動
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
---
+++ popup.js
@@ -0,0 +1,197 @@
+// var bkg = chrome.extension.getBackgroundPage();
+const stopWord = [
+    "科技", "股份", "公司", "有限", "企業",
+    "集團", "國際", "家庭", "online", "網路",
+    "股", "資訊", "服務", "社", "分公司", "台灣"
+];
+
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // console.log('tabs', tabs);
+    var tab = tabs[0];
+    tab_title = tab.title;
+    chrome.tabs.executeScript(tab.id, {
+        code: 'document.querySelector("h1").textContent'
+    }, serviceManager);
+});
+
+function serviceManager(query) {
+    let newQuery = processQuery(query);
+    search_soft_job(newQuery);
+    search_tech_job(newQuery);
+}
+
+function processQuery(query) {
+    // 去除標點符號，與變換成小寫
+    query = query.toString().toLowerCase().replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g, " ")
+    // bkg.console.log('not process query: ' + query);
+    for (let word of stopWord) {
+        query = query.toString().replace(word, '');
+    }
+    return query;
+}
+
+function search_soft_job(query) {
+
+    // let english_query = query.toString().replace(/[^A-Za-z0-9]+/g, ' ');
+    // let chinese_query = query.toString().replace(/[^\u4e00-\u9fa5]/g, '');
+
+    // 預設查詢
+    let queryElement = document.getElementById("Job Query");
+    queryElement.value = query.replace(/\s/g, '');
+    // bkg.console.log('job query: ' + query);
+
+    // request 
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == XMLHttpRequest.DONE) {
+            let loader = document.getElementById("Soft Job Loader");
+            draw_Soft_Compoment(xmlHttp.responseText);
+            loader.setAttribute("style", "display:none");
+        }
+    }
+    xmlHttp.onerror = function(e) {
+        let originSoftJobResult = document.getElementById("Soft Job Content");
+        originSoftJobResult.innerHTML = "herokuapp request error";
+    };
+    xmlHttp.open("GET", "https://www.ptt.cc/bbs/Soft_Job/search?q=" + query, true); // false for synchronous request
+    xmlHttp.send(null);
+}
+
+function draw_Soft_Compoment(htmlText) {
+    // 每筆回傳結果
+    var response = document.implementation.createHTMLDocument().documentElement;
+    response.innerHTML = htmlText;
+    let array = response.getElementsByClassName("r-ent");
+
+    // create UI
+    let parent = document.getElementById("Soft Job Content");
+    for (let index = 0; index < array.length; index++) {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        let cardContainer = document.createElement("div");
+        cardContainer.setAttribute("class", "container");
+
+        let a = document.createElement("a");
+        let br = document.createElement("br");
+        var title = document.createTextNode(array[index].getElementsByClassName("title")[0].textContent.replace(/\s/g, ''));
+        a.setAttribute("href", "https://www.ptt.cc/" + array[index].getElementsByClassName("title")[0].getElementsByTagName("a")[0].getAttribute("href").replace(/\s/g, ''));
+        a.setAttribute("target", "_blank");
+        a.appendChild(br);
+        a.appendChild(title);
+
+        let p = document.createElement("p");
+        p.textContent = "推文數：" + array[index].getElementsByClassName("nrec")[0].textContent.replace(/\s/g, '') + " 日期：" + array[index].getElementsByClassName("date")[0].textContent.replace(/\s/g, '');
+
+        cardContainer.appendChild(a);
+        cardContainer.appendChild(p);
+        card.appendChild(cardContainer);
+        parent.appendChild(card);
+    }
+}
+
+function search_tech_job(query) {
+
+    // 預設查詢
+    let queryElement = document.getElementById("Job Query");
+    queryElement.value = query.replace(/\s/g, '');
+    // bkg.console.log('job query: ' + query);
+
+    // request 
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == XMLHttpRequest.DONE) {
+            let loader = document.getElementById("Tech Job Loader");
+            loader.setAttribute("style", "display:none");
+            draw_Tech_Compoment(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.onerror = function(e) {
+        let originTechJobResult = document.getElementById("Tech Job Content");
+        originTechJobResult.innerHTML = "herokuapp request error";
+    };
+    xmlHttp.open("GET", "https://www.ptt.cc/bbs/Tech_Job/search?q=" + query, true); // false for synchronous request
+    xmlHttp.send(null);
+}
+
+function draw_Tech_Compoment(htmlText) {
+    // 每筆回傳結果
+    var response = document.implementation.createHTMLDocument().documentElement;
+    response.innerHTML = htmlText;
+    let array = response.getElementsByClassName("r-ent");
+
+    // create UI
+    let parent = document.getElementById("Tech Job Content");
+    for (let index = 0; index < array.length; index++) {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        let cardContainer = document.createElement("div");
+        cardContainer.setAttribute("class", "container");
+
+        let a = document.createElement("a");
+        let br = document.createElement("br");
+        var title = document.createTextNode(array[index].getElementsByClassName("title")[0].textContent.replace(/\s/g, ''));
+        a.setAttribute("href", "https://www.ptt.cc/" + array[index].getElementsByClassName("title")[0].getElementsByTagName("a")[0].getAttribute("href").replace(/\s/g, ''));
+        a.setAttribute("target", "_blank");
+        a.appendChild(br);
+        a.appendChild(title);
+
+        let p = document.createElement("p");
+        p.textContent = "推文數：" + array[index].getElementsByClassName("nrec")[0].textContent.replace(/\s/g, '') + " 日期：" + array[index].getElementsByClassName("date")[0].textContent.replace(/\s/g, '');
+
+        cardContainer.appendChild(a);
+        cardContainer.appendChild(p);
+        card.appendChild(cardContainer);
+        parent.appendChild(card);
+    }
+}
+
+// 設定 tab button
+document.addEventListener('DOMContentLoaded', function () {
+    var softButton = document.getElementById('Soft Job Tab');
+    var techButton = document.getElementById('Tech Job Tab');
+    var softContent = document.getElementById('Soft Job');
+    var techContent = document.getElementById('Tech Job');
+    // onClick's logic below:
+    softButton.addEventListener('click', function () {
+        softContent.style.display = "block";
+        techContent.style.display = "none";
+        softButton.className += " active";
+        techButton.className = techButton.className.replace(" active", "");
+    });
+    techButton.addEventListener('click', function () {
+        techContent.style.display = "block";
+        softContent.style.display = "none";
+        techButton.className += " active";
+        softButton.className = softButton.className.replace(" active", "");
+    });
+
+    // 預設先開啟 softbutton
+    softButton.click();
+
+    let researchJobButton = document.getElementById("Research Job");
+    researchJobButton.addEventListener('click', function () {
+        researchJob();
+    });
+});
+
+function researchJob() {
+    if(event.key === 'Enter') {
+        alert(ele.value);        
+    }
+    let newQuery = document.getElementById("Job Query").value;
+
+    let originSoftJobResult = document.getElementById("Soft Job Content");
+    let originTechJobResult = document.getElementById("Tech Job Content");
+
+    let softLoader = document.getElementById("Soft Job Loader");
+    let techLoader = document.getElementById("Tech Job Loader");
+
+    originSoftJobResult.innerHTML = "";
+    originTechJobResult.innerHTML = "";
+    softLoader.setAttribute("style", "display:block");
+    techLoader.setAttribute("style", "display:block");
+
+    search_soft_job(newQuery);
+    search_tech_job(newQuery);
+}
